7f2ea57871096edcb47ec79ea5a8151be3319173
José Magalhães <magalhas@gmail.com>
2013-04-25 22:24:47 -0400

    decoupled audio from metadata into two services

--- routers/tracks.js
+++ routers/tracks.js
@@ -9,7 +9,7 @@ var
   express = require("express"),
   fs = require("fs-extra"),
   MusicMetadata = require("musicmetadata"),
-  passport = require("passport"),
+  Q = require("q"),
   TrackModel = require("../models/track");
 /**
  * @class module:routers/tracks~TracksRouter
@@ -45,7 +45,11 @@ TracksRouter.prototype.getTrackFromDropbox = function (track, options) {
     },
     function (err, buffer) {
       if (err) {
-        options.error && options.error(err);
+        if (options.error) {
+          options.error(err);
+        } else {
+          throw err;
+        }
       } else {
         options.success && options.success(buffer);
       }
@@ -59,53 +63,53 @@ TracksRouter.prototype.getTrackFromDropbox = function (track, options) {
 TracksRouter.prototype.routeGetTrackById = function (req, res) {
   var
     self = this,
-    path,
     trackId = req.param("id");
   // Get the track info from the database
-  this.app.log("Getting track %s.", trackId);
-  TrackModel.findById(trackId, function (err, track) {
-    if (err) {
-      self.app.error("Error: %j", err);
+  Q
+    .ninvoke(TrackModel, "findById", trackId)
+    .then(function (track) {
+      res.send(track);
+    })
+    .fail(function (err) {
       res.status(500).send(err);
-    } else {
-      res.type("audio/mpeg");
-      // If the track is cached
-      if (self.app.cache.exists(track.path, track.modifiedAt)) {
-        // Send the track to the client
-        self.app.log("Sending track '%s' to client.", track.path);
-        path = self.app.cache.path(track.path, track.modifiedAt);
-        res.sendfile(path);
-      }
-      // If the track is not cached
-      else {
-        self.getTrackFromDropbox(track, {
-          error: function (err) {
-            self.app.error("Error: %j", err);
-            res.status(500).send(err);
-          },
-          success: function (buffer) {
-            self.app.log("Caching and sending track to client.");
-            path = self.app.cache.store(track.path, track.modifiedAt, buffer);
-            self.storeMetadata(path, track, {
-              error: function (err) {
-                self.app.error("Error: %j", err);
-                res.status(500).send(err);
-              },
-              success: function () {
-                res.sendfile(path);
-              }
-            });
-          }
-        });
-      }
-    }
-  });
+    });
 };
 /**
  * @route
  * @todo Documentation.
  */
 TracksRouter.prototype.routeGetTrackAudioById = function (req, res) {
+  var
+    self = this,
+    path,
+    trackId = req.param("id");
+  res.type("audio/mpeg");
+  // Get the track info from the database
+  Q
+    .ninvoke(TrackModel, "findById", trackId)
+    .then(function (track) {
+      if (self.app.cache.exists(track.path, track.modifiedAt)) {
+        path = self.app.cache.path(track.path, track.modifiedAt);
+        res.sendfile(path);
+      } else {
+        self.getTrackFromDropbox(track, {
+          success: function (buffer) {
+            path = self.app.cache.store(track.path, track.modifiedAt, buffer);
+            self
+              .storeMetadata(path, track)
+              .then(function () {
+                res.sendfile(path);
+              })
+              .done();
+          }
+        });
+      }
+    })
+    .fail(function (err) {
+      self.app.error(err);
+      res.status(500).send(err);
+    })
+    .done();
 };
 /**
  * @route
@@ -113,23 +117,23 @@ TracksRouter.prototype.routeGetTrackAudioById = function (req, res) {
  */
 TracksRouter.prototype.routeGetTracks = function (req, res) {
   var self = this;
-  TrackModel.find({}, function (err, tracks) {
-    if (err) {
-      self.app.error("There was an error getting tracks. Error: %j", err);
-      res.status(500).send(err);
-    } else {
+  Q
+    .ninvoke(TrackModel, "find", {})
+    .then(function (tracks) {
       res.send(tracks);
-    }
-  });
+    })
+    .fail(function (err) {
+      res.status(500).send(err);
+    })
+    .done();
 };
 /**
  * @todo Documentation.
  */
-TracksRouter.prototype.storeMetadata = function (path, track, options) {
+TracksRouter.prototype.storeMetadata = function (path, track) {
   var
     id3 = {},
     musicMetadata = new MusicMetadata(fs.createReadStream(path));
-  !options && (options = {});
   musicMetadata.on("metadata", function (metadata) {
     id3.artist = metadata.artist;
     id3.album = metadata.album;
@@ -141,14 +145,10 @@ TracksRouter.prototype.storeMetadata = function (path, track, options) {
   musicMetadata.on("TLEN", function (duration) {
     id3.duration = duration;
   });
-  musicMetadata.on("done", function (err) {
-    TrackModel.update({_id: track._id}, id3, {upsert: true}, function (err) {
-      if (err) {
-        options.error && options.error(err);
-      } else {
-        options.success && options.success();
-      }
+  return Q
+    .ninvoke(musicMetadata, "on", "done")
+    .then(function () {
+      return Q.ninvoke(TrackModel, "update", {_id: track._id}, id3, {upsert: true});
     });
-  });
 };
 exports = module.exports = TracksRouter;
